8c3c4de80ae6069dd84e4ff7534d69988b817b0a
BenKamperman <ben.kamperman@okstate.edu>
2023-02-08 17:01:40 -0600

    Add project files.

---
+++ BK Basic App GitHub/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BK_Basic_App_GitHub
+{
+    public class Class1 {
+        public String Name;
+        public Class1()
+        {
+            Name = "[Ben Kamperman] - The Code Master";
+        }
+    }
+}
---
+++ Basic App Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Basic App Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Basic App Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8eb8da0c-21ef-4ce1-9de1-72be3cfca44b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
